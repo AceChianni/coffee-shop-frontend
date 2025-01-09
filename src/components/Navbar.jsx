0f5168debd167be7fa82f994a9a6048e8c0146f1
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2025-01-09 15:31:55 +0000

    styles edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,16 @@
-// /src/components/Navbar.js
-import Link from 'next/link';
-import styles from '../styles/navbar.module.css';  
+// src/components/Navbar.jsx
+
+import styles from '../styles/navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className={`${styles.navbar} fixed top-0 left-0 w-full shadow-md z-50`}>
-      <div className="container mx-auto flex justify-between items-center p-4">
-        {/* Navbar Links */}
-        <div className="flex space-x-8">
-          {/* Home Link */}
-          <Link href="/" className={`${styles.link} hover:text-secondary transition duration-300`}>
-            Home
-          </Link>
-          <Link href="/products" className={`${styles.link} hover:text-secondary transition duration-300`}>
-            Menu
-          </Link>
-          <Link href="/cart" className={`${styles.link} hover:text-secondary transition duration-300`}>
-            Cart
-          </Link>
-          <Link href="/signup" className={`${styles.link} hover:text-secondary transition duration-300`}>
-            Sign Up
-          </Link>
-          <Link href="/signin" className={`${styles.link} hover:text-secondary transition duration-300`}>
-            Sign In
-          </Link>
-        </div>
+    <nav className={styles.navbar}>
+      <div className={styles.links}>
+        <a className={styles.link} href="/">Home</a>
+        <a className={styles.link} href="/products">Menu</a>
+        <a className={styles.link} href="/cart">Cart</a>
+        <a className={styles.link} href="/signin">Sign In</a>
+        <a className={styles.link} href="/signup">Sign Up</a>
       </div>
     </nav>
   );
