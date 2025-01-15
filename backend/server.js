7772b522a45911b9c26fbccb25495517b998753a
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2025-01-15 16:53:26 +0000

    update server for mongodb_uri

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
+  console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
